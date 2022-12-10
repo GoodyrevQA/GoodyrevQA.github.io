9dc61f80b0e7ee21018c8dd31c30ac35f7c92806
Иван Гудырев <119449219+GoodyrevQA@users.noreply.github.com>
2022-12-10 20:28:24 +0300

    Add files via upload
    Сайт-визитка на HTML+CSS+JS

---
+++ js.js
@@ -0,0 +1,13 @@
+var button1 = document.getElementById('button1');
+button1.addEventListener('click', function() {
+	swal("Умею:", "• HTTP, REST, SOAP, JSON, XML\n • Devtools, Mobile Devtools, Charles\n • Postman\n • Qase.io, Miro\n • HTML, CSS, JS\n • Linux, Git, CI/CD\n • PostgreSQL, MySQL\n • Python+Pytest+Requests / Python+Selenium+Allure\n • Cypress JS\n • Jmeter\n • CRUD, Agile\n ...\n • the list is being updated", {
+		button: "NOT BAD",
+	  });
+})
+
+var button2 = document.getElementById('button2');
+button2.addEventListener('click', function() {
+	swal("FAC SI FACIS", "If you're going to do it, do it!", {
+		button: "OK, let's do it",
+	  });
+})
