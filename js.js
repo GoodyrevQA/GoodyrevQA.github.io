07adc42e911db6fd2757a58abd2cd7bb38f83d7c
Ivan_Gudyrev <iegudyrev@gmail.com>
2022-12-11 16:35:37 +0300

    stepik

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var button1 = document.getElementById('button1');
 button1.addEventListener('click', function() {
-	swal("Умею:", "• HTTP, REST, SOAP, JSON, XML\n • Devtools, Mobile Devtools, Charles\n • Postman\n • Qase.io, Miro\n • HTML, CSS, JS\n • Linux, Git, CI/CD\n • PostgreSQL, MySQL\n • Python+Pytest+Requests / Python+Selenium+Allure\n • Cypress JS\n • Jmeter\n • CRUD, Agile\n ...\n • the list is being updated", {
+	swal("Умею:", "• HTTP, REST, SOAP, JSON, XML\n • Devtools, Mobile Devtools, Charles\n • Postman\n • Qase.io, Miro\n • HTML, CSS, JS\n • Linux, Git, CI/CD\n • PostgreSQL, MySQL\n • Python+Pytest+Requests / Python+Selenium+Allure\n • Cypress JS\n • Jmeter\n • CRUD, Agile\n ...\n • The list is being updated", {
 		button: "NOT BAD",
 	  });
 })
